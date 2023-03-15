2484f9223d658fb80a6bf13281aa6909bad4adc8
Michael <michael.d.nath@gmail.com>
2023-03-14 18:53:27 -0700

    introduced deep rl stuff

--- env/dining_hall_env.py
+++ env/dining_hall_env.py
@@ -2,7 +2,8 @@ from env.env_utils import *
 from env.dining_hall import DiningHall
 from env.diner import Diner
 import numpy as np
-import gymnasium as gym
+# import gymnasium as gym
+import gym
 
 # DINING_HALL_OPENING_HOUR = 5
 # DINING_HALL_CLOSING_HOUR = 10
@@ -20,6 +21,14 @@ class DiningHallEnv(gym.Env):
                  reward_function=reward_short_waits,
                  render_mode=None
                  ):
+        super(DiningHallEnv, self).__init__()
+        self.action_space = gym.spaces.MultiDiscrete(np.full(num_diners, num_dining_halls))
+        self.observation_space = gym.spaces.Dict(
+            {
+                "current_day": gym.spaces.Discrete(num_dining_days),
+                "unavailable_halls": gym.spaces.MultiDiscrete(np.full(num_diners, num_dining_halls))
+             }
+            )
         # assert num_dining_days > 0 and num_dining_halls > 0
         self.num_halls = num_dining_halls
         self.day_tracker = {'current_day': start_day,
@@ -30,6 +39,7 @@ class DiningHallEnv(gym.Env):
         self.setup_diners(num_diners, num_dining_times, dining_time_func)
         self.unvailable_halls = np.full(num_diners, -1)
         self.setup_halls()
+        self.special_probs = np.array([dining_hall.special_probs for dining_hall in self.dining_halls])
 
     def setup_diners(self, num_diners, num_dining_time_choices, dining_time_func):
         dining_times = np.sort(dining_time_func(
@@ -49,8 +59,7 @@ class DiningHallEnv(gym.Env):
             self.dining_halls.append(new_hall)
 
     def get_obs(self):
-        special_probs = np.array([dining_hall.special_probs for dining_hall in self.dining_halls])
-        return {'day': self.day_tracker['current_day'], 'unavailable_halls': self.unvailable_halls, "special_probs":special_probs}
+        return {'current_day': self.day_tracker['current_day'], 'unavailable_halls': self.unvailable_halls}
 
     def reset(self, start_day=0, options=None):
         for dining_hall in self.dining_halls:
@@ -59,7 +68,6 @@ class DiningHallEnv(gym.Env):
         return self.get_obs()
 
     def step(self, a):
-        obs = self.get_obs()
         done = None
         step_reward = 0
         for idx, diner in enumerate(self.diners):
@@ -68,6 +76,7 @@ class DiningHallEnv(gym.Env):
             diner.visit_dining_hall(a[idx])
             chosen_hall = self.dining_halls[a[idx]]
             diner_reward = self.reward_function(chosen_hall.headcount, chosen_hall.special, visited_hall_twice)
+            self.unvailable_halls[idx] = a[idx]
             step_reward += diner_reward
             self.dining_halls[a[idx]].increment_headcount()
 
@@ -76,7 +85,9 @@ class DiningHallEnv(gym.Env):
         if not done:
             for dining_hall in self.dining_halls:
                 dining_hall.prep_for_next_day()
-        return obs, step_reward, done
+        info = {"special_probs": self.special_probs}
+        obs = self.get_obs()
+        return obs, step_reward, done, info
 
     def render(self):
         raise NotImplementedError
